8f893b45a6bc75ca2de4559e41023942aa116e39
asinugobi <asinugo@seas.upenn.edu>
2017-11-23 08:39:35 -0500

    build new model with additional emoji features

--- final_project_kit/naive_bayes/naive_bayes.m
+++ final_project_kit/naive_bayes/naive_bayes.m
@@ -9,16 +9,21 @@ load('train.mat')
 %% Preprocess the data. 
 % Convert X_train_bag into feature matrix (sparse x)
 sparse_x = full(X_train_bag); 
+
+% Grab emojis 
+emoji_matrix = get_emojis(sparse_x);
+
+% Convert sparse matrix to binary features
 sparse_x(sparse_x > 1) = 1;
+
+% Add additional emoji counts to feature matrix 
+sparse_x(:, m+1:m+size(emoji_matrix, 2)) = emoji_matrix; 
 
 %% Train the tree model.
 % Train across Naive Bayes model. 
 tic 
 nb_model = fitcnb(sparse_x,Y_train, 'Distribution', 'mn'); 
 toc 
-
-% Compute the predicted labels from training data. 
-% Y_fit = predict(nb_model, sparse_x); 
 
 % Compute the training error (loss) 
 general_loss = loss(nb_model, sparse_x, Y_train); 
