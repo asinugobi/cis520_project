c758712f847c4b96f53ff8d1f357ab2b657d9c2e
asinugobi <asinugo@seas.upenn.edu>
2017-12-02 20:57:50 -0500

    fixed nb label predictions

--- final_project_kit/naive_bayes/predict_labels.m
+++ final_project_kit/naive_bayes/predict_labels.m
@@ -2,16 +2,16 @@ function [Y_hat] = predict_labels(X_test_bag, test_raw)
 
 % Inputs:   X_test_bag     nx10000 bag of words features
 %           test_raw      nx1 cells containing all the raw tweets in text 
-% load('naive_bayes.mat') 
+load('naive_bayes.mat') 
 
 %% Preprocess the data. 
 % Convert X_train_bag into feature matrix (sparse x)
-sparse_x = full(X_train_bag); 
+sparse_x = full(X_test_bag); 
 sparse_x(sparse_x > 1) = 1;
 
-remove_features = [136 79]; 
-
-sparse_x(:, remove_features) = [];  
+% remove_features = [136 79]; 
+% 
+% sparse_x(:, remove_features) = [];  
 
 % Outputs:  Y_hat nx1 predicted labels (1 for joy, 2 for sadness, 3 for surprise, 4 for anger, 5 for fear)
 [Y_hat_model, posterior, costs_model] = predict(nb_model, sparse_x);  
